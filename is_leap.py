6b4989b1ad74036ca0a342547e094cf3d626fb77
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-20 22:06:00 +0530

    Create is_leap.py

---
+++ is_leap.py
@@ -0,0 +1,6 @@
+# we are going to define a list of program to check whether it is leap year or not
+def is_leap(leap):
+    # if number is divide by 4 gives 0 then it is leap year
+    if(leap%4==0):
+        print("It is leap year")
+is_leap(2028)
