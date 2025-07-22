88bdcf40d2fe2352aaa4f3b0e0560bcfa5bb2a02
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-22 17:41:52 +0530

    Create larg_amng_thr.py

---
+++ larg_amng_thr.py
@@ -0,0 +1,8 @@
+# Program to find largest among three numbers
+
+# creating a function that will find the largest number from the list
+
+def find_larg(list):
+     return max(list)
+# This function will return the list
+print(f"The largest number among 3 number is: {find_larg([22,34,43])}")
