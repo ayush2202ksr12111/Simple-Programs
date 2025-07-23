ee799e9287531be23dfb749429c81b6447286426
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-23 17:47:20 +0530

    Create cnv_dec_oth.py

---
+++ cnv_dec_oth.py
@@ -0,0 +1,11 @@
+# now we are going to see and convert the decimal to binary, octal, hexadecimal
+
+def dec_conv(dec):
+    print(f"Decimal to binary of{dec} is {bin(dec)}")
+
+    print(f"Decimal to octal of{dec} is {oct(dec)}")
+
+    print(f"Decimal to hexa deciaml of{dec} is {hex(dec)}")
+    
+
+dec_conv(25)
