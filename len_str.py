97200e130e8cd6e304a474e4c518bbcb1ebc93b2
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-28 17:39:10 +0530

    Create len_str.py

---
+++ len_str.py
@@ -0,0 +1,7 @@
+# python program to count the number of digits present in a number
+
+# first we are going to create the function that will convert the numbers into string and the string will just return the number present in it
+def len_num(num):
+    return len(str(num))
+    
+print(len_num(3323323))
